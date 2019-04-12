f70cdf661bbc852a5759f05e327eb2b4392a49f0
Bulat <sadikovba@gmail.com>
2019-04-12 16:33:44 +0300

    Transform Swapi data

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -13,21 +13,23 @@ export default class SwapiService {
 	}
 
 	async getAllPeople () {
-		const res = await this.getResource(`/people/`)
-		return res.results;
+		const res = await this.getResource(`/people/`);
+		return res.results.map(this._transformPerson);
 	}
 
-	getPerson (id) {
-		return this.getResource(`/people/${id}`)
+	async getPerson (id) {
+		const person = await this.getResource(`/people/${id}`);
+		return this._transformPerson(person);
 	}
 
 	async getAllPlanets () {
 		const res = await this.getResource(`/planets/`)
-		return res.results;
+		return res.results.map(this._transformPlanet);
 	}
 
-	getPlanet (id) {
-		return this.getResource(`/planets/${id}`)
+	async getPlanet (id) {
+		const planet = await this.getResource(`/planets/${id}`);
+		return this._transformPlanet(planet);
 	}
 
 	async getAllStarships () {
@@ -35,8 +37,48 @@ export default class SwapiService {
 		return res.results;
 	}
 
-	getStarhip (id) {
-		return this.getResource(`/starships/${id}`)
+	async getStarhip (id) {
+		const starship = this.getResource(`/starships/${id}`);
+		return this._transformStarhip(starship);
+	}
+
+	_extractId(item) {
+		const idRegExp = /\/([0-9]*)\/$/;
+		return item.url.match(idRegExp)[1];
+	}
+
+	_transformPlanet(planet) {
+		return {
+			id: this._extractId(planet),
+			name: planet.name,
+			population: planet.population,
+			rotationPeriod: planet.rotation_period,
+			diameter: planet.diameter
+		}
+	}
+
+	_transformStarhip(starship) {
+		return {
+			id: this._extractId(starship),
+			name: starship.name,
+			model: starship.model,
+			manufacturer: starship.manufacturer,
+			costInCredits: starship.cost_in_credits,
+			length: starship.length,
+			crew: starship.crew,
+			passengers: starship.passengers,
+			cargoCapacity: starship.cargo_capacity
+		}
+	}
+
+	_transformPerson(person) {
+		return {
+			id: this._extractId(person),
+			name: person.name,
+			gender: person.gender,
+			birthYear: person.birth_year,
+			eyeColor: person.eyeColor
+		}
 	}
 
 }
