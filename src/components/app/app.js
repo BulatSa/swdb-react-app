e3872507745e99209aca6fc75cd1f5652bcb5e05
Bulat <sadikovba@gmail.com>
2019-04-16 11:30:18 +0300

    Add componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,35 +6,36 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
 
 	state = {
-		selectedPerson: null,
-		loading: true
+		loading: true,
+		hasError: false
 	};
 
-	onPersonSelected = (id) => {
-		this.setState({
-			selectedPerson: id,
-			loading: true
-		});
-	};
+
+
+	componentDidCatch(error, errorInfo) {
+		console.log('componentDidCatch()');
+		this.setState({ hasError: true });
+	}
 
 	render() {
+
+		if (this.state.hasError) {
+			return <ErrorIndicator />
+		}
+
 		return (
 			<div className='container'>
 				<Header />
 				<RandomPlanet />
-
-				<div className="row mb2">
-					<div className="col-md-6">
-						<ItemList onItemSelected={this.onPersonSelected}/>
-					</div>
-					<div className="col-md-6">
-						<PersonDetails personId={this.state.selectedPerson} />
-					</div>
-				</div>
+				<PeoplePage />
+				<PeoplePage />
+				<PeoplePage />
 			</div>
 		);
 	}
